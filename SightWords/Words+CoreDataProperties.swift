fc2b6d2d60e908308c105f9c99cd497c5183e4d0
devshutdown <devshutdown@gmail.com>
2016-12-05 21:12:45 -0600

    Update Words+CoreDataProperties.swift

--- SightWords/Words+CoreDataProperties.swift
+++ SightWords/Words+CoreDataProperties.swift
@@ -2,8 +2,8 @@
 //  Words+CoreDataProperties.swift
 //  MyWordss
 //
-//  Created by cis290 on 10/10/16.
-//  Copyright © 2016 Rock Valley College. All rights reserved.
+//  Created by Aaron Goodin
+//  Copyright © 2016 Aaron Goodin
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
